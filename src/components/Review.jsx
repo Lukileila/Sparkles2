2eb2669289359c5918e3cd450a576a2f5588882a
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-10-09 17:27:56 +0200

    review design changed

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -4,8 +4,8 @@ export default function Review () {
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
   <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked/>
-  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" />
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400"/>
+  <input type="radio" name="rating-2" className="mask mask-star-2 bg-orange-400" checked />
 </div>
     )
 }
