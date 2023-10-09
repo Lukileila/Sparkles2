9341e1158f7455f7dd14428c02d5e80f9982da31
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-10-09 16:07:39 +0200

    some image changes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,10 @@
-import logo from '../assets/images/sparkleslogo.png';
+
 import {Link} from 'react-router-dom';
 
 export default function Card(){    
     return (
         <div className="card w-96 bg-base-100 shadow-xl mb-8">
-        <figure><img src={logo} alt="Shoes" /></figure>
+        <figure><img src='https://wallpapers.com/images/hd/comic-book-background-a09lfl6egxpsqgwc.jpg' alt="Shoes" /></figure>
         <div className="card-body">
           <h2 className="card-title">Shoes!</h2>
           <p>If a dog chews shoes whose shoes does he choose?</p>
--- src/components/HeroBanner.jsx
+++ src/components/HeroBanner.jsx
@@ -1,6 +1,8 @@
+import HeroImage from '../assets/images/heroimage.jpeg';
+
 export default function HeroBanner(){    
     return (
-        <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+        <div className="hero min-h-screen" style={{backgroundImage: 'url(https://wallpapers.com/images/hd/comic-book-background-a09lfl6egxpsqgwc.jpg)'}}>
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
