b36566a101cbc67a66f1c4333fb8fbc4fad03fa6
Harun Kohen Erdeniz <110021233+kohkohshine@users.noreply.github.com>
2023-10-10 17:55:08 +0200

    contentful content

---
+++ src/contentful.js
@@ -0,0 +1,23 @@
+import { createClient } from 'contentful';
+
+const client = createClient({
+  space: "lerti9id4rxk",                                       
+    /*  import.meta.env.VITE_SPACE_ID, */
+  accessToken: "wxR7_bR8_nE62t7-zKSB9fTUNEKFfQmGiiiWNZXJJco"                       
+    /* import.meta.env.VITE_ACCESS_TOKEN, */
+});
+
+const getEntries = async () => {
+  try {
+    const getSomeEntries = await client.getEntries({
+      content_type: 'pageProduct',
+    });
+
+    console.log(getSomeEntries.items);
+    return getSomeEntries.items;
+  } catch (error) {
+    console.error(error.message);
+  }
+};
+
+export { getEntries };
